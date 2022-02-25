1960454c80733c60a3d33741b53a385dfa569965
iRuxu <rx6@qq.com>
2022-02-25 15:56:12 +0800

    feat:nav init

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="m-nav">
-        <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+        <el-tree :data="data" :props="defaultProps" node-key="key" :default-expanded-keys="['world']">
+            <router-link class="el-tree-node__label" slot-scope="{ data }" to="/">
+                <span class="u-name" v-text="data.label"></span>
+                <em v-if="data.count" class="u-count" v-text="`(${data.count})`"></em>
+            </router-link>
+        </el-tree>
     </div>
 </template>
 
@@ -12,7 +17,6 @@ export default {
     data: function () {
         return {
             data: [
-
                 // TODO:构建2019~现在及未来的数据
                 {
                     label: "日历",
@@ -31,6 +35,7 @@ export default {
                 // TODO:关联书籍接口
                 {
                     label: "书籍",
+                    key: "book",
                     children: [
                         {
                             label: "杂集",
@@ -70,33 +75,38 @@ export default {
                 // TODO:关联接口
                 {
                     label: "江湖",
+                    key: "world",
                     children: [
                         {
-                            label: "事件",//（包含新闻，正规的）
+                            label: "事件",
+                            key: "bigbang",
                         },
                         {
-                            label: "八卦",//八卦（不正经的）
+                            label: "八卦",
+                            key: "gossip",
                         },
                         {
                             label: "玩法",
+                            key: "game",
                         },
                         {
                             label: "术语",
+                            key: "jargon",
                         },
                         {
                             label: "玩家",
+                            key: "player",
                         },
                         {
                             label: "组织",
+                            key: "organization",
                         },
                         {
                             label: "其它",
+                            key: "other",
                         },
                     ],
                 },
-
-
-
             ],
             defaultProps: {
                 children: "children",
