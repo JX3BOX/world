4b328ff24f8151ab4c4772db9038258390966292
Kaviilee <jiaweilee95@126.com>
2022-03-03 18:47:27 +0800

    feat: frontend calendar

---
+++ src/router/calendar.js
@@ -0,0 +1,16 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+Vue.use(VueRouter);
+
+const home = () => import('@/views/calendar/Calendar.vue')
+
+
+const routes = [
+    { name: 'home', path: '/', component: home }
+];
+
+const router = new VueRouter({
+    routes,
+});
+
+export default router
---
+++ src/views/calendar/index.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="app">
+        <Header></Header>
+        <Breadcrumb name="剑三日历" slug="calendar" root="/calendar" :feedbackEnable="true" :crumbEnable="false">
+            <img slot="logo" svg-inline :src="getAppIcon('calendar')" />
+        </Breadcrumb>
+        <LeftSidebar>
+            <Nav />
+        </LeftSidebar>
+        <Main :withoutRight="true">
+            <div class="m-main">
+                <router-view />
+            </div>
+            <Footer></Footer>
+        </Main>
+    </div>
+</template>
+
+<script>
+import { getAppIcon } from "@jx3box/jx3box-common/js/utils";
+import Nav from "@/components/Nav.vue";
+export default {
+    name: "App",
+    props: [],
+    data: function () {
+        return {};
+    },
+    computed: {},
+    methods: { getAppIcon },
+    components: { Nav },
+    mounted: function () {},
+};
+</script>
+
+<style lang="less">
+@import "~@/assets/css/app.less";
+@import "~@/assets/css/calendar/index.less";
+</style>
