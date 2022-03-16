6503acb9bd9675981fb350ec662d98b78ad34782
Kaviilee <jiaweilee95@126.com>
2022-03-16 13:38:56 +0800

    perf: remove lambad

--- src/service/calendar.js
+++ src/service/calendar.js
@@ -1,55 +1,46 @@
-import { $cms } from '@jx3box/jx3box-common/js/https'
+import { $cms } from "@jx3box/jx3box-common/js/https";
 
-const $ = $cms()
-
-const genUrl = (params) => {
+function genUrl(params) {
     const { year, month, date } = params;
 
-    let url = '';
+    let url = "";
 
-    year && (url += `/${year}`)
-    month && (url += `/${month}`)
-    date && (url += `/${date}`)
+    year && (url += `/${year}`);
+    month && (url += `/${month}`);
+    date && (url += `/${date}`);
 
-    return url
+    return url;
 }
 
-const getCalendar = (params) => {
-
+function getCalendar(params) {
     let url = genUrl(params);
 
-    return $.get('/api/cms/calendar/archive' + url)
+    return $cms().get("/api/cms/calendar/archive" + url);
 }
 
-const addCalendar = (data) => {
-    const url = genUrl(data);
-    return $.post('/api/cms/calendar/archive' + url, data)
+function addCalendar(params, data) {
+    const url = genUrl(params);
+
+    return $cms().post("/api/cms/calendar/archive" + url, data);
 }
 
-const putCalendar = (id, data) => {
-    return $.put(`/api/cms/calendar/${id}`, data)
+function putCalendar(id, data) {
+    return $cms().put(`/api/cms/calendar/${id}`, data);
 }
 
-const delCalendar = (id) => {
-    return $.delete(`/api/cms/calendar/${id}`)
+function delCalendar(id) {
+    return $cms().delete(`/api/cms/calendar/${id}`);
 }
 
-const getDetail = (id) => {
-    return $.get(`/api/cms/calendar/${id}`)
+function getDetail(id) {
+    return $cms().get(`/api/cms/calendar/${id}`);
 }
 
-const getHistory = (params, query) => {
-    let url = genUrl(params)
-    return $.get(`/api/cms/calendar/history/${url}`, {
-        params: query
-    })
+function getHistory(params, query) {
+    let url = genUrl(params);
+    return $cms().get(`/api/cms/calendar/history/${url}`, {
+        params: query,
+    });
 }
 
-export {
-    getCalendar,
-    addCalendar,
-    putCalendar,
-    delCalendar,
-    getDetail,
-    getHistory,
-}
+export { getCalendar, addCalendar, putCalendar, delCalendar, getDetail, getHistory };
