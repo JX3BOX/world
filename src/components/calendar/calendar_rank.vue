39c49c2c6b97276d1333027436d74b7819775c6e
zixuan1986 <litthy@qq.com>
2022-04-15 19:26:23 +0800

    fix:将排行榜单独提取出来 #15

--- src/components/calendar/calendar_rank.vue
+++ src/components/calendar/calendar_rank.vue
@@ -1,35 +1,35 @@
 <template>
     <el-dialog custom-class="m-calendar-rank-dialog" :visible.sync="show" width="30%" center :before-close="handleClose">
         <span slot="title" class="u-rank-title"><i class="el-icon-s-data"></i> 剑三日历贡献排行榜 </span>
-        <el-divider>【 {{date.start}} ~ {{date.end}} 】</el-divider>
-        <ul class="m-list">
-            <li v-for="(item,index) in list" :key="index">
-                <span class="u-number"> <img :src="rankImg(index+1)" alt="" srcset="" v-if="index <3"><span v-else>{{index+1}}</span> </span>
-                <img class="u-avatar" :src="item.user.user_avatar || avatar" :alt="item.user.display_name">
-                <span class="u-name">{{item.user.display_name}}</span>
-                <span class="u-num"><b>{{item.count}} </b>条</span>
-            </li>
-        </ul>
+        <div v-loading="loading">
+            <el-divider>【 {{date.start}} ~ {{date.end}} 】</el-divider>
+            <ul class="m-list">
+                <li v-for="(item,index) in list" :key="index">
+                    <span class="u-number"> <img :src="rankImg(index+1)" alt="" srcset="" v-if="index <3"><span v-else>{{index+1}}</span> </span>
+                    <img class="u-avatar" :src="item.user.user_avatar || avatar" :alt="item.user.display_name">
+                    <span class="u-name">{{item.user.display_name}}</span>
+                    <span class="u-num"><b>{{item.count}} </b>条</span>
+                </li>
+            </ul>
+        </div>
     </el-dialog>
+
 </template>
 <script>
 import { __imgPath } from "@jx3box/jx3box-common/data/jx3box.json";
+import { getCalendarRank, getRankDate } from "@/service/calendar.js";
 export default {
     name: "calendarRank",
-    props: ["data"],
+    props: [""],
     data: function () {
-        return {};
+        return {
+            list: [],
+            date: "",
+            show: true,
+            loading: false,
+        };
     },
     computed: {
-        list() {
-            return this.data.list;
-        },
-        date() {
-            return this.data.date;
-        },
-        show() {
-            return this.data.show;
-        },
         avatar() {
             return `${__imgPath}image/other/avatar.png`;
         },
@@ -41,6 +41,31 @@ export default {
         rankImg(id) {
             return `${__imgPath}image/other/rank_${id}.png`;
         },
+        // 排行榜数据
+        getRankData() {
+            this.loading = true;
+            getRankDate()
+                .then((res) => {
+                    if (!res.data.data) return;
+                    let data = {};
+                    res.data.data.forEach((item) => {
+                        if (item.key == "calendar_rank_start") data.start = item.val;
+                        if (item.key == "calendar_rank_end") data.end = item.val;
+                    });
+                    this.date = data;
+                    if (data.start && data.end)
+                        getCalendarRank(this.date).then((res) => {
+                            if (!res.data.data) return;
+                            this.list = res.data.data.list;
+                        });
+                })
+                .finally(() => {
+                    this.loading = false;
+                });
+        },
+    },
+    created() {
+        this.getRankData();
     },
 };
 </script>
