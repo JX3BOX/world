5e7dd0b764b358ca35b72922eb76e72511f3fd7e
Kaviilee <jiaweilee95@126.com>
2022-12-21 17:57:05 +0800

    perf: update

--- src/components/reputation/nav.vue
+++ src/components/reputation/nav.vue
@@ -1,30 +1,14 @@
 <template>
-  <nav class="m-nav">
-    <div class="m-menus-panel">
-      <el-tree
-        class="m-menus"
-        :data="versions"
-        node-key="id"
-        ref="tree"
-      >
-        <router-link
-          class="el-tree-node__label"
-          slot-scope="{ node, data }"
-          :to="menuLink(data, node)"
-        >
-          <span
-            class="u-name"
-            v-text="'【 ' + data.name + ' 】'"
-          ></span>
-          <em
-            v-if="data.total"
-            class="u-count"
-            v-text="`(${data.total})`"
-          ></em>
-        </router-link>
-      </el-tree>
-    </div>
-  </nav>
+    <nav class="m-nav">
+        <div class="m-menus-panel">
+            <el-tree class="m-menus" :data="versions" node-key="id" ref="tree">
+                <router-link class="el-tree-node__label" slot-scope="{ node, data }" :to="menuLink(data, node)">
+                    <span class="u-name" v-text="'【 ' + data.name + ' 】'"></span>
+                    <em v-if="data.total" class="u-count" v-text="`(${data.total})`"></em>
+                </router-link>
+            </el-tree>
+        </div>
+    </nav>
 </template>
 
 <script>
@@ -32,45 +16,46 @@ import { getMenus } from "@/service/reputation";
 import maps from "@jx3box/jx3box-data/data/fb/fb_map.json";
 
 export default {
-  name: "Nav",
-  data: () => ({
-    keyword: "",
-    menus: [],
-    versions: []
-  }),
-  computed: {
-    client () {
-      return this.$store.state.client;
+    name: "Nav",
+    data: () => ({
+        keyword: "",
+        menus: [],
+        versions: [],
+    }),
+    computed: {
+        client() {
+            return this.$store.state.client;
+        },
     },
-  },
-  methods: {
-    menuLink (menu) {
-      return { name: "result", query: { id: menu.nDlcID } };
+    methods: {
+        menuLink(menu) {
+            return { name: "result", query: { id: menu.nDlcID } };
+        },
+        getMenus() {
+            getMenus({}).then((res) => {
+                // console.log(res)
+                const list = res.data.dlc || [];
+                const arr = Object.keys(maps).reverse();
+                arr.unshift("声望总览");
+                this.versions = list.map((item, i) => {
+                    if (!item.nDlcID) {
+                        item.total = list
+                            .map((lItem) => lItem.total)
+                            .reduce(function (prev, cur) {
+                                return prev + cur;
+                            });
+                    }
+                    return {
+                        ...item,
+                        name: arr[i],
+                    };
+                });
+            });
+        },
     },
-    getMenus () {
-      getMenus({
-      }).then((res) => {
-        // console.log(res)
-        const list = res.data.dlc || []
-        const arr = Object.keys(maps).reverse()
-        arr.unshift('声望总览')
-        this.versions = list.map((item, i) => {
-          if (!item.nDlcID) {
-            item.total = list.map(lItem => lItem.total).reduce(function (prev, cur) {
-              return prev + cur
-            })
-          }
-          return {
-            ...item,
-            name: arr[i]
-          }
-        })
-      });
+    mounted() {
+        this.getMenus();
     },
-  },
-  mounted () {
-    this.getMenus();
-  },
 };
 </script>
 
--- src/components/reputation/single/reward_item.vue
+++ src/components/reputation/single/reward_item.vue
@@ -1,39 +1,37 @@
 <template>
-  <div class="reputation-reward-item">
-    <div class="reputation-reward-title">{{reward.level}}</div>
-    <div class="reputation-reward-list">
-      <item-icon
-        v-for="(reward, index) in reward.list"
-        :key="index"
-        :item_id="reward.id"
-        :amount="reward.amount"
-        :onlyIcon="true"
-        :size="28"
-        style="margin-right: 5px;margin-bottom: 5px"
-      ></item-icon>
+    <div class="reputation-reward-item">
+        <div class="reputation-reward-title">{{ reward.level }}</div>
+        <div class="reputation-reward-list">
+            <item-icon
+                v-for="(reward, index) in reward.list"
+                :key="index"
+                :item_id="reward.id"
+                :amount="reward.amount"
+                :onlyIcon="true"
+                :size="28"
+                class="u-item-icon"
+            ></item-icon>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-import ItemIcon from '../common/item_icon.vue'
+import ItemIcon from "../common/item_icon.vue";
 export default {
-  name: "RewardItem",
-  props: {
-    reward: {
-      type: Object,
+    name: "RewardItem",
+    props: {
+        reward: {
+            type: Object,
+        },
     },
-  },
-  components: {
-    ItemIcon
-  },
-  data () {
-    return {
-    };
-  },
-  methods: {},
-  computed: {
-  },
+    components: {
+        ItemIcon,
+    },
+    data() {
+        return {};
+    },
+    methods: {},
+    computed: {},
 };
 </script>
 
@@ -52,7 +50,10 @@ export default {
         padding-top: 10px;
         padding-bottom: 10px;
         max-height: 88px;
-        overflow-y: scroll;
+        overflow-y: auto;
+    }
+    .u-item-icon {
+        margin: 0 5px 5px 0;
     }
 }
 </style>
